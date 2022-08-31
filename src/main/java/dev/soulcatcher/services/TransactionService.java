25431320170a76c808a8ec87b6b55fd4571d07da
Richard Moch <ibmpcdos5@gmail.com>
2022-08-31 11:26:04 -0500

    create principal and login request dtos

--- src/main/java/dev/soulcatcher/services/TransactionService.java
+++ src/main/java/dev/soulcatcher/services/TransactionService.java
@@ -3,6 +3,7 @@ package dev.soulcatcher.services;
 import dev.soulcatcher.models.Account;
 import dev.soulcatcher.models.Transaction;
 import dev.soulcatcher.repos.TransactionRepository;
+import dev.soulcatcher.util.Generation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +25,7 @@ public class TransactionService {
     public void addTransaction(double amount, String merchant, Account account) {
         Transaction transaction = new Transaction();
         transaction.setAccount(account);
-        transaction.setTransactionId(UUID.randomUUID().toString());
+        transaction.setTransactionId(Generation.genId());
         transaction.setAmount(amount);
         transaction.setMerchant(merchant);
         account.getTransactions().add(transaction);
